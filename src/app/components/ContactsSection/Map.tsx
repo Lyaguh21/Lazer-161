6dbbdf45d60e4fda66f37d61f8a2aac5848aa3e9
Rrailee <vlad107a@gmail.com>
2025-02-22 18:14:24 +0300

    yandexmap

--- src/app/components/ContactsSection/Map.tsx
+++ src/app/components/ContactsSection/Map.tsx
@@ -1,3 +1,11 @@
 export default function Map() {
-  return <div className="h-[540px] w-full laptop:w-[560px] bg-slate-900"></div>;
+  return (
+    <div className="h-[540px] w-full laptop:w-[560px]">
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A9ab915425113695f4860978090a102200cee02a358eac7fba119c63c05e94bd1&amp;source=constructor"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </div>
+  );
 }
