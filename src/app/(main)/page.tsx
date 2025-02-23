00091099ff07cdada6f6d760309f8088709be6c6
Rrailee <vlad107a@gmail.com>
2025-02-23 15:12:47 +0300

    DemoSlider

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,5 @@
 import ContactsSection from "../components/ContactsSection/ContactsSection";
+import ExampleSection from "../components/ExampleWork/ExampleSection";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header />
       <div className="w-full h-[1000px] bg-red-200"></div>
+      <ExampleSection />
       <ContactsSection />
       <Footer />
     </>
